7115d16375686adca5126f1782d3fea4bd802085
yurdzhunk <yurdzhunk@gmail.com>
2022-01-10 22:03:08 +0200

    frontend app is started with installed node packages, babel, webpack, react and index.html created

--- product_market/settings.py
+++ product_market/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store.apps.StoreConfig',
-    'rest_framework'
+    'rest_framework',
+    'frontend.apps.FrontendConfig'
     
 ]
 
--- product_market/urls.py
+++ product_market/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('store.urls'))
+    path('store', include('store.urls')),
+    path('', include('frontend.urls'))
 ]
